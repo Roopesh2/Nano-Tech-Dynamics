a6712d4f8c818c6735dbdd2e1504e9300b854489
Roopesh <roopeshor6@gmail.com>
2021-11-13 07:53:23 +0530

    added more sections

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,6 +8,7 @@ module.exports = merge(common, {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
   },
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
