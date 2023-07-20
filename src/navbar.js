b4610897d764205355f147a593ed541219628b23
Roopesh <roopeshor6@gmail.com>
2023-07-20 08:33:39 +0530

    reworked front page

--- src/navbar.js
+++ src/navbar.js
@@ -1,8 +1,12 @@
 const hamburgerBtn = document.getElementById("hamburger");
+const closeIcon = document.getElementById("close");
+const openIcon = document.getElementById("open");
 const navMenu = document.querySelector(".menu");
 
 function toggleHamburger() {
   navMenu.classList.toggle("show");
+  closeIcon.classList.toggle("show");
+  openIcon.classList.toggle("show");
 }
 
 // toggle hamburger menu
