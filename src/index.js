18e00fe7d89f8bc2363a212d65b3f110dc830005
Roopesh O R <62459820+Roopesh2@users.noreply.github.com>
2024-07-13 15:46:09 +0530

    updated product details
    Signed-off-by: Roopesh O R <62459820+Roopesh2@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -12,43 +12,43 @@ const products = [
 	},
 	{
 		"file": "huv-antenna.webp",
-		"name": "High Frequency UHV Compatible Conical Antenna (Surface Finish Up To 0.4μm)"
+		"name": "High Frequency UHV Compatible Conical Antenna <br/>Surface Finish Up To 0.4μm Ra"
 	},
 	{
 		"file": "s-band.webp",
-		"name": "S Band Mode Converter Wave Guide (Surface Finish Up To 0.4μm)"
+		"name": "S Band Mode Converter Wave Guide - UHV Compatible <br/>Surface Finish Up To 0.4μm Ra"
 	},
 	{
 		"file": "s-band-90-c.webp",
-		"name": "S Band 90 Degree Circular Bend Wave Guide"
+		"name": "S Band 90 Degree Circular Bend Wave Guide - UHV Compatible 1 meter <br/>Surface Finish Up To 0.4μm Ra"
+	},
+	{
+		"file": "huv-antenna-2.webp",
+		"name": "Conical Antenna - High Frequency UHV Compatible 𝜙90-400 mm x L350 mm"
 	},
 	{
 		"file": "s-band-90-r.webp",
-		"name": "S Band 90 Degree Rectangular Bend Wave Guide"
+		"name": "S Band 90 Degree Rectangular Bend Wave Guide - UHV Compatible 1 meter"
 	},
 	{
 		"file": "antenna-3.webp",
-		"name": "High Frequency UHV Compatible Wave Guide"
+		"name": "High Frequency UHV Compatible Wave Guide 𝜙90 x L500 mm/1000 mm <br/>Surface Finish Up To 0.4μm Ra"
 	},
 	{
 		"file": "s-band-r.jpg",
-		"name": "S Band Rectangular Wave Guide"
-	},
-	{
-		"file": "product2.png",
-		"name": "S Band Antenna For Defence Purpose"
+		"name": "S Band Rectangular Wave Guide UHV Compatible L-1 meter <br/>Surface Finish Up To 0.4μm Ra"
 	},
 	{
 		"file": "product4.webp",
-		"name": "Micro Reactor Used For Defence Sector"
+		"name": "Micro Reactor"
 	},
 	{
 		"file": "product5.webp",
 		"name": "Super Finished Mode Converter"
 	},
 	{
-		"file": "product6.jpg",
-		"name": "Micro Dialysis Setup For Usa"
+		"file": "product6.png",
+		"name": "Micro Dialysis Setup For USA"
 	}
 ]
 
@@ -67,7 +67,7 @@ window.addEventListener("load", () => {
 		productsGrid.innerHTML += `
 <div class="card">
 	<img src="./assets/products/${product.file}" alt="" class="card-img" />
-	<h3 class="card-title">${product.name}</h3>
+	<h4 class="card-title" style="font-weight: 600;">${product.name}</h3>
 </div>
 		`
 	}
